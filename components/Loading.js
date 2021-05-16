1cc9c093ef526e34ce8bca17b66b781a152972a1
Fayaz Basheer <pnfayaz@gmail.com>
2021-05-16 17:34:00 +0400

    loading changed

--- components/Loading.js
+++ components/Loading.js
@@ -1,12 +1,10 @@
+import { Circle } from 'better-react-spinkit'
+
 function Loading() {
   return (
     <center style={{ display: 'grid', placeItems: 'center', height: '100vh' }}>
       <div>
-        <p color='#3CBC38' size={60}>
-         
-          Whatsapp is loading ...{' '}
-       {' '}
-        </p>
+        <Circle color='#3CBC38' size={60} />
       </div>
     </center>
   )
